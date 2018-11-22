8e8dd240ace9afcd4326cd021ad48daa5c2b5bc6
Lương <luongfrv2522@gmail.com>
2018-11-22 23:57:13 +0700

    update phieu nghiem thu

--- view/backend/view_add_edit_thongtinhoidong_nghiemthu.php
+++ view/backend/view_add_edit_thongtinhoidong_nghiemthu.php
@@ -94,6 +94,40 @@
         </div>
         <!-- /page content -->
 
+<script type="text/javascript">
+  function URL_add_parameter(url, param, value){
+    var hash       = {};
+    var parser     = document.createElement('a');
+
+    parser.href    = url;
+
+    var parameters = parser.search.split(/\?|&/);
+
+    for(var i=0; i < parameters.length; i++) {
+        if(!parameters[i])
+            continue;
+
+        var ary      = parameters[i].split('=');
+        hash[ary[0]] = ary[1];
+    }
+
+    hash[param] = value;
+
+    var list = [];  
+    Object.keys(hash).forEach(function (key) {
+        list.push(key + '=' + hash[key]);
+    });
+
+    parser.search = '?' + list.join('&');
+    return parser.href;
+}
+  $(function(){
+    $('select.form-control[name="fk_mabomon_id"]').on('change',function(){
+      //console.log($(this).val());
+      window.location.href = URL_add_parameter(window.location.href,'fk_mabomon_id',$(this).val());
+    });
+  });
+</script>
 
 
 
