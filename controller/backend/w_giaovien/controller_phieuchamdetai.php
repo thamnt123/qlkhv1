6284941d4e39e9eff05adb1c469f71a085a22572
Lương <luongfrv2522@gmail.com>
2018-11-23 01:05:18 +0700

    phieu cham theo userr

--- controller/backend/w_giaovien/controller_phieuchamdetai.php
+++ controller/backend/w_giaovien/controller_phieuchamdetai.php
@@ -8,7 +8,8 @@
 			//quy dinh so ban ghi hien thi tren mot trang
 			$record_per_page = 5;
 			//tinh tong so ban ghi
-			$total = $this->model->num_rows("SELECT * from tbl_detai_phieucham dtpc JOIN tbl_detai dt ON dt.pk_madetai_id = dtpc.fk_madetai_id JOIN tbl_user u ON u.pk_user_id = dt.fk_user_id JOIN tbl_bomon bm ON bm.pk_mabomon_id = u.fk_mabomon_id");
+			$pk_user_id = $_SESSION['SS_USER']->pk_user_id;
+			$total = $this->model->num_rows("SELECT * from tbl_detai_phieucham dtpc JOIN tbl_detai dt ON dt.pk_madetai_id = dtpc.fk_madetai_id JOIN tbl_user u ON u.pk_user_id = dt.fk_user_id JOIN tbl_bomon bm ON bm.pk_mabomon_id = u.fk_mabomon_id where dt.fk_user_id =$pk_user_id or EXISTS (SELECT * from tbl_detai_user where fk_madetai_id = dt.pk_madetai_id and fk_user_id=$pk_user_id) or dt.fk_user_id in(SELECT fk_user_id from tbl_hoidong_detai where fk_madetai_id = dt.pk_madetai_id and fk_user_id=$pk_user_id)");
 			//tinh so trang
 			$num_page = ceil($total/$record_per_page);
 			//lay bien p truyen tu url, bien nay se chi trang hien tai
@@ -17,7 +18,7 @@
 			$from = $p * $record_per_page;			
 			//---------
 			//lay toan bo ban ghi co phan trang
-			$arr = $this->model->get_all("SELECT * from tbl_detai_phieucham dtpc JOIN tbl_detai dt ON dt.pk_madetai_id = dtpc.fk_madetai_id JOIN tbl_user u ON u.pk_user_id = dt.fk_user_id JOIN tbl_bomon bm ON bm.pk_mabomon_id = u.fk_mabomon_id order by dt.pk_madetai_id desc limit $from,$record_per_page");
+			$arr = $this->model->get_all("SELECT * from tbl_detai_phieucham dtpc JOIN tbl_detai dt ON dt.pk_madetai_id = dtpc.fk_madetai_id JOIN tbl_user u ON u.pk_user_id = dt.fk_user_id JOIN tbl_bomon bm ON bm.pk_mabomon_id = u.fk_mabomon_id where dt.fk_user_id =$pk_user_id or EXISTS (SELECT * from tbl_detai_user where fk_madetai_id = dt.pk_madetai_id and fk_user_id=$pk_user_id) or dt.fk_user_id in(SELECT fk_user_id from tbl_hoidong_detai where fk_madetai_id = dt.pk_madetai_id and fk_user_id=$pk_user_id) order by dt.pk_madetai_id desc limit $from,$record_per_page");
 			//load view
 			include "view/backend/w_giaovien/view_phieuchamdetai.php";
 		}
